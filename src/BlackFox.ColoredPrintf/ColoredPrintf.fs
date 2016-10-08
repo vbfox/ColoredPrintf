ac14c9f4bff9f8068714fccec5c7da47ee01ed91
Julien Roncaglia <julien@roncaglia.fr>
2016-10-08 22:50:33 +0200

    Code cleanup

--- src/BlackFox.ColoredPrintf/ColoredPrintf.fs
+++ src/BlackFox.ColoredPrintf/ColoredPrintf.fs
@@ -42,7 +42,7 @@ module ColoredString =
         ]
         |> Map.ofSeq
 
-    let colorNameToColor (name: string) = colorIdentifiers |> Map.tryFind(name.ToLowerInvariant())
+    let inline private colorNameToColor (name: string) = colorIdentifiers |> Map.tryFind(name.ToLowerInvariant())
 
     module private StateHelpers =
         let inline clearText (state: WriterState) = ignore(state.CurrentText.Clear())
@@ -60,7 +60,7 @@ module ColoredString =
 
     open StateHelpers
 
-    let writeChar (env: IColoredPrinterEnv) (state: WriterState) (c: char) =
+    let writeChar (env: IColoredPrinterEnv) (c: char) (state: WriterState) =
         match state.Status with
         | WriterStatus.Normal when c = '$' ->
             writeCurrentTextToEnv env state
@@ -97,6 +97,10 @@ module ColoredString =
             state.Status <- WriterStatus.Normal            
         | WriterStatus.Background -> state |> appendChar c
        
+    let writeString (env: IColoredPrinterEnv) (s: string) (state: WriterState) =
+        for i in 0..s.Length-1 do
+            state |> writeChar env (s.[i])
+
     let finish (env: IColoredPrinterEnv) (state: WriterState) =
         match state.Status with
         | WriterStatus.Normal ->
@@ -112,108 +116,8 @@ module ColoredString =
         let initialBg = env.Background
         
         let state = getEmptyState ()
-        for i in 0..s.Length-1 do writeChar env state (s.[i])
+        state |> writeString env s
         
-        finish env state
+        state |> finish env
         if initialFg <> env.Foreground then env.Foreground <- initialFg
         if initialBg <> env.Background then env.Background <- initialBg
-(*
-[<RequireQualifiedAccess>]
-[<CompilationRepresentation(CompilationRepresentationFlags.ModuleSuffix)>]
-module ColoredString =
-    open System
-    open System.Text
-
-    module Option =
-        let orDefault default' option = defaultArg option default'
-
-    let private colorIdentifiers =
-        [
-            yield "reset", Reset
-            for color in Enum.GetValues(typedefof<ConsoleColor>) |> Seq.cast<ConsoleColor> do
-                yield color.ToString().ToLowerInvariant(), Color(color)
-        ]
-        |> Map.ofSeq
-
-    let private tryParseColor (s:string) =
-        colorIdentifiers |> Map.tryFind (s.ToLowerInvariant()) |> Option.orDefault NoColor
-
-    let private parseColorCodes (s:string) =
-        let split = s.Split(';')
-
-        let foreground = if split.Length >= 1 then tryParseColor split.[0] else NoColor
-        let background = if split.Length >= 2 then tryParseColor split.[1] else NoColor
-
-        (foreground, background)
-
-    
-
-
-    
-    let length = fold (fun x _ -> x + 1) (fun x _ -> x) 0
-
-    let create raw = { Raw = raw }
-
-    let toString coloredString = 
-        let builder = coloredString |> fold (fun (b:StringBuilder) c -> b.Append c) (fun b _ -> b) (new StringBuilder())
-        builder.ToString()
-
-    let private writeCore (inner: (ColorIdentifier -> ColorIdentifier -> unit) -> unit) =
-        let initalForeground = Console.ForegroundColor
-        let initialBackground = Console.BackgroundColor
-
-        let setForeground = function
-            | NoColor -> ()
-            | Reset -> Console.ForegroundColor <- initalForeground
-            | Color color -> Console.ForegroundColor <- color
-
-        let setBackground = function
-            | NoColor -> ()
-            | Reset -> Console.BackgroundColor <- initialBackground
-            | Color color -> Console.BackgroundColor <- color
-
-        let setColors foreground background =
-            setForeground foreground
-            setBackground background
-                        
-        inner setColors
-
-        setColors Reset Reset
-
-    let writeToConsole coloredString = 
-        let doWrite setColors =
-            fold
-                (fun _ c -> Console.Write(c))
-                (fun _ (foreground, background) -> setColors foreground background)
-                ()
-                coloredString
-
-        writeCore doWrite
-
-    let writeConsoleStartingAtPosition position coloredString =
-        let doWrite setColors =
-            let writeChar i (c:char) =
-                if i < position then
-                    i + 1
-                else
-                    Console.Write c
-                    i + 1
-
-            let setColors' i (foreground, background) =
-                setColors foreground background
-                i
-
-            fold writeChar setColors' 0 coloredString |> ignore
-
-        writeCore doWrite
-
-[<AutoOpen>]
-module ColoredStringAutoOpen =
-    open System
-
-    let coloredWrite = ColoredString.create >> ColoredString.writeToConsole
-
-    let coloredWriteLine s =
-        coloredWrite s
-        Console.WriteLine()
-*)
