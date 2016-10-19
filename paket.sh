df17073b01201d8486fb2021e345757402f024d2
Julien Roncaglia <julien@roncaglia.fr>
2016-10-19 22:43:45 +0200

    Use bleeding edge un-released paket bootstrapper
    https://github.com/fsprojects/Paket/pull/1961

--- paket.sh
+++ paket.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
-
-PAKET_VERSION=3.21.4
-
 function dotnet { if test "$OS" = "Windows_NT"; then $@; else mono $@; fi }
-
-dotnet .paket/paket.bootstrapper.exe -s $PAKET_VERSION  || { exit $?; }
-dotnet .paket/paket.exe $@
+dotnet paket.exe $@
