bc682e11cee1fdfa897f886f8b5ad64d0c277c7b
Julien Roncaglia <julien@roncaglia.fr>
2024-09-05 23:16:25 +0200

    Better interpolated string usage

--- src/TestApp/Program.fs
+++ src/TestApp/Program.fs
@@ -2,11 +2,18 @@
 open BlackFox.ColoredPrintf
 
 [<EntryPoint>]
-let main argv = 
+let main argv =
     colorprintfn "Hello $red[world]."
     colorprintfn "Hello $green[%s]." "user"
     colorprintfn "$white[Progress]: $yellow[%.2f%%] (Eta $yellow[%i] minutes)" 42.33 5
     colorprintfn "$white;blue[%s ]$black;white[%s ]$white;red[%s]" "La vie" "est" "belle"
+
+    let life = "La vie"
+    let is_ = "est"
+    colorprintfn $"""$white;blue[%s{life} ]$black;white[{is_} ]$white;red[{"belle"}]"""
+
     colorprintfn "Hello $%A;%A[world] !" ConsoleColor.Magenta ConsoleColor.Yellow
+
+    colorprintfn $"Hello $%A{ConsoleColor.Magenta};{ConsoleColor.Yellow}[world] !"
     ignore(Console.ReadLine())
     0 // return an integer exit code
