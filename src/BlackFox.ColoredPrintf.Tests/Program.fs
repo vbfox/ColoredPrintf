d9690a75b0de26b84602a6973bc23f253cc00426
Julien Roncaglia <julien@roncaglia.fr>
2026-08-02 12:04:56 +0200

    Update package versions

--- src/BlackFox.ColoredPrintf.Tests/Program.fs
+++ src/BlackFox.ColoredPrintf.Tests/Program.fs
@@ -1,7 +1,6 @@
 ﻿open Expecto
+open Expecto.Tests
 
 [<EntryPoint>]
 let main args =
-    let writeResults = TestResults.writeNUnitSummary "TestResults.xml"
-    let config = defaultConfig.appendSummaryHandler writeResults
-    runTestsInAssembly config args
+    runTestsInAssemblyWithCLIArgs [ NUnit_Summary "TestResults.xml" ] args
